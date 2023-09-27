868ab597a4c12e117fae4d33d8514c623c022b63
federico501 <federicasiu.123@gmail.com>
2023-09-27 00:00:19 -0500

    se agrega firebase para la autenticacion del user

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCDO3falfYzT2jYl6tWdKbYnbVMzQ4aY8Y",
+  authDomain: "bookapp-af12f.firebaseapp.com",
+  projectId: "bookapp-af12f",
+  storageBucket: "bookapp-af12f.appspot.com",
+  messagingSenderId: "360071287113",
+  appId: "1:360071287113:web:9b874c2416874abbc50a98"
+};
+
