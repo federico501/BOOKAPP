068e218bd0227394258a44b96287fab3469bfa1f
federico501 <federicasiu.123@gmail.com>
2023-09-28 17:19:13 -0500

    se conecta a la api open library para buscar los libros

--- screens/SearchBooksScreen.js
+++ screens/SearchBooksScreen.js
@@ -1,15 +1,77 @@
-// SearchBooksScreen.js
-import React from 'react';
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ImageBackground, TextInput, Button, Image, Dimensions, ScrollView } from 'react-native';
+import axios from 'axios';
+const screenWidth = Dimensions.get('window').width;
 
 const SearchBooksScreen = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = () => {
+    axios.get(`https://openlibrary.org/search.json?q=${searchQuery}`)
+      .then((response) => {
+        const books = response.data.docs.map((book) => ({
+          title: book.title,
+          coverUrl: `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`,
+          key: book.key,
+        }));
+        setSearchResults(books);
+      })
+      .catch((error) => {
+        console.error('Error al buscar libros', error);
+      });
+  };
+
+
+  // Dividir los resultados en filas de tres elementos
+  const chunkArray = (arr, chunkSize) => {
+    const chunkedArray = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      chunkedArray.push(arr.slice(i, i + chunkSize));
+    }
+    return chunkedArray;
+  };
+
+  const resultsChunks = chunkArray(searchResults, 3);
+
   return (
     <ImageBackground
-      source={require('../assets/Screen2.png')} // Ruta de la imagen de fondo
+      source={require('../assets/Screen2.png')}
       style={styles.container}
     >
       <View style={styles.content}>
         <Text style={styles.title}>Buscar Libros</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Ingrese el título del libro"
+          value={searchQuery}
+          onChangeText={(text) => setSearchQuery(text)}
+        />
+        <Button
+          title="Buscar"
+          onPress={handleSearch}
+        />
+        <ScrollView>
+          {resultsChunks.map((row, rowIndex) => (
+            <View key={rowIndex} style={styles.row}>
+              {row.map((item) => (
+                <View style={styles.bookContainer} key={item.key}>
+                  <Image
+                    style={styles.coverImage}
+                    source={{ uri: item.coverUrl }}
+                  />
+                  <Text style={styles.resultText}>{item.title}</Text>
+                  <Button
+                    title="Leer"
+                    onPress={() => {
+                      Linking.openURL(`https://openlibrary.org${item.key}`);
+                    }}
+                  />
+                </View>
+              ))}
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </ImageBackground>
   );
@@ -24,7 +86,7 @@ const styles = StyleSheet.create({
   },
   content: {
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    padding: 20,
+    padding: 30,
     borderRadius: 10,
     alignItems: 'center',
   },
@@ -33,6 +95,32 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
     marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    backgroundColor: 'white',
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  bookContainer: {
+    width: screenWidth / 3 - 20, // Ajusta el ancho para tres elementos por fila
+    alignItems: 'center',
+    marginBottom: 20, // Espacio vertical entre las filas de libros
+  },
+  coverImage: {
+    width: 100,
+    height: 150,
+  },
+  resultText: {
+    color: 'white',
+    marginBottom: 10,
+    textAlign: 'center',
   },
 });
 
