bea067fded9b23adc121a733dffc2b80163f7ce0
federico501 <federicasiu.123@gmail.com>
2023-10-02 00:30:53 -0500

    se añade una api para mostrar libros aleatoriamente

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,28 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity, ScrollView, Modal, FlatList, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { getFirestore, collection, doc, getDoc } from 'firebase/firestore'; // Importa las funciones de Firestore
+import axios from 'axios'; // Importa axios para realizar solicitudes HTTP
+import { getFirestore, doc, getDoc } from 'firebase/firestore'; // Importa las funciones de Firestore
 
 const HomeScreen = ({ navigation }) => {
   const [userName, setUserName] = useState('');
+  const [randomBooks, setRandomBooks] = useState([]);
+  const [selectedBook, setSelectedBook] = useState(null);
 
   useEffect(() => {
-    const db = getFirestore();
-    const userDocRef = doc(db, 'Usuario', 'ID_DEL_USUARIO'); // Reemplaza 'ID_DEL_USUARIO' con el ID real del usuario
-
-    const fetchUserData = async () => {
+    // Función para obtener libros aleatorios desde la API de Google Books
+    const getRandomBooks = async () => {
       try {
+        const response = await axios.get(
+          'https://www.googleapis.com/books/v1/volumes?q=random&maxResults=10'
+        );
+        const books = response.data.items || [];
+        setRandomBooks(books);
+      } catch (error) {
+        console.error('Error al obtener libros aleatorios:', error);
+      }
+    };
+
+    // Llama a la función para obtener libros aleatorios cuando se carga la pantalla
+    getRandomBooks();
+
+    // Función para obtener el nombre del usuario desde Firestore
+    const fetchUserName = async () => {
+      try {
+        const db = getFirestore();
+        const userDocRef = doc(db, 'Usuario', 'ID_DEL_USUARIO'); // Reemplaza 'ID_DEL_USUARIO' con el ID real del usuario
         const userDocSnapshot = await getDoc(userDocRef);
+
         if (userDocSnapshot.exists()) {
           const userData = userDocSnapshot.data();
           setUserName(userData.name); // Suponiendo que el campo de nombre en Firestore se llama 'name'
         }
       } catch (error) {
-        console.error('Error al cargar los datos del usuario:', error);
+        console.error('Error al cargar el nombre del usuario:', error);
       }
     };
 
-    fetchUserData();
+    // Llama a la función para obtener el nombre del usuario cuando se carga la pantalla
+    fetchUserName();
+
   }, []);
 
   const handleSearchBooks = () => {
@@ -37,6 +59,30 @@ const HomeScreen = ({ navigation }) => {
     navigation.navigate('AddBooks');
   };
 
+  const openBookModal = (book) => {
+    setSelectedBook(book);
+  };
+
+  const closeBookModal = () => {
+    setSelectedBook(null);
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.bookItem}
+      onPress={() => openBookModal(item)}
+    >
+      <Image
+        source={{ uri: item.volumeInfo.imageLinks?.thumbnail || 'https://via.placeholder.com/150' }}
+        style={styles.bookCover}
+      />
+      <Text style={styles.bookTitle}>{item.volumeInfo.title}</Text>
+      <Text style={styles.bookAuthor}>
+        {item.volumeInfo.authors ? item.volumeInfo.authors.join(', ') : 'Autor Desconocido'}
+      </Text>
+    </TouchableOpacity>
+  );
+
   return (
     <ImageBackground
       source={require('../assets/Sceen1.png')}
@@ -45,6 +91,40 @@ const HomeScreen = ({ navigation }) => {
       <View style={styles.header}>
         <Text style={styles.userName}>Bienvenido, {userName}</Text>
       </View>
+
+      <FlatList
+        data={randomBooks}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        numColumns={2} // Organiza los libros en 2 columnas
+        contentContainerStyle={styles.bookList}
+      />
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={selectedBook !== null}
+        onRequestClose={closeBookModal}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            {selectedBook && (
+              <ScrollView>
+                <Text style={styles.modalTitle}>{selectedBook.volumeInfo.title}</Text>
+                <Text style={styles.modalAuthor}>
+                  {selectedBook.volumeInfo.authors ? selectedBook.volumeInfo.authors.join(', ') : 'Autor Desconocido'}
+                </Text>
+                <Text style={styles.modalDescription}>
+                  {selectedBook.volumeInfo.description || 'Sin descripción disponible.'}
+                </Text>
+                <TouchableOpacity style={styles.closeButton} onPress={closeBookModal}>
+                  <Ionicons name="close" size={32} color="white" />
+                </TouchableOpacity>
+              </ScrollView>
+            )}
+          </View>
+        </View>
+      </Modal>
 
       <View style={styles.bottomBar}>
         <TouchableOpacity style={styles.iconButton} onPress={handleSearchBooks}>
@@ -82,6 +162,64 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
     marginBottom: 20,
+  },
+  bookList: {
+    padding: 16,
+  },
+  bookItem: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    borderRadius: 10,
+    padding: 10,
+    margin: 8,
+  },
+  bookCover: {
+    width: 150,
+    height: 200,
+    resizeMode: 'cover',
+    borderRadius: 5,
+  },
+  bookTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+    marginTop: 5,
+  },
+  bookAuthor: {
+    fontSize: 14,
+    color: 'white',
+    marginTop: 5,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  modalContent: {
+    backgroundColor: 'rgba(0, 0, 0, 0.9)',
+    borderRadius: 10,
+    padding: 20,
+    width: '80%',
+    maxHeight: '80%',
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  modalAuthor: {
+    fontSize: 18,
+    color: 'white',
+    marginTop: 5,
+  },
+  modalDescription: {
+    fontSize: 16,
+    color: 'white',
+    marginTop: 10,
+  },
+  closeButton: {
+    alignItems: 'flex-end',
   },
   bottomBar: {
     flexDirection: 'row',
